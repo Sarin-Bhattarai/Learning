40895542b7ff9e010be8a51c5e3fb6c86be71e62
sarin punk <sarin.bhattarai@icp.edu.np>
2024-05-29 14:53:26 +0545

    feat(initiliazing the layout)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import { useState } from "react";
 
 export default function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list"> LIST </div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X(X%)</em>
+    </footer>
+  );
 }
 
 //for understanding the state and components-project 2(Steps: Next, Previous and Close)
