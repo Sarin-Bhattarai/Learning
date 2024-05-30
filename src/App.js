db0c19c2b1a63764432d1eaf52cc4a416c82102d
sarin punk <sarin.bhattarai@icp.edu.np>
2024-05-30 10:12:59 +0545

    feat(rendering the list)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -24,7 +30,27 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list"> LIST </div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        <span>{item.quantity}</span>
+        <span> {item.description}</span>
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
