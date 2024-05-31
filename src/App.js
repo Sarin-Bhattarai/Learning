11fbfe5e4335645adc7f30a7f6c53bd47ea9e354
sarin punk <sarin.bhattarai@icp.edu.np>
2024-05-31 10:33:32 +0545

    feat(form handling)

--- src/App.js
+++ src/App.js
@@ -22,10 +22,47 @@ function Logo() {
 }
 
 function Form() {
+  //controlled elements
+  //step 1: create a piece of state
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault(); //to prevent the reloading while submitting the form
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))} //since it is number we have to convert the value as number
+      >
+        {/* using Array.form() to create a array of 20 elements as number and use the value for option field dynamically*/}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        value={description} //step 2: use the state as value for particular field
+        onChange={(e) => setDescription(e.target.value)} //step 3: use the onChange for updating the value for field using setState
+        placeholder="Item..."
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -34,7 +71,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
