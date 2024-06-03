9ff3d800b7d1002dbe2a1fd3eca0fbf6aecd8a99
sarin punk <sarin.bhattarai@icp.edu.np>
2024-06-03 09:50:03 +0545

    feat(displaying the added items)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]); //simply pushing the new items to array
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,10 +25,10 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   //controlled elements
   //step 1: create a piece of state
-  const [description, setDescription] = useState("");
+  const [description, setDescription] = useState(""); //example of local state
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
@@ -37,6 +41,7 @@ function Form() {
       packed: false,
       id: Date.now(),
     };
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -66,11 +71,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
